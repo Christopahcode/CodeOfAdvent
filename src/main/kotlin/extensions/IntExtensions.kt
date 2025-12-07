44d333c449fbc8df915267b22745149c604681b0
Christopher Gabrielsen <christopahcode@gmail.com>
2025-12-07 23:04:07 +0100

    Day-02: Added extension function that will split any even number in half into a Pair

--- src/main/kotlin/extensions/IntExtensions.kt
+++ src/main/kotlin/extensions/IntExtensions.kt
@@ -2,3 +2,9 @@ package extensions
 import kotlin.math.abs
 
 infix fun Int.distanceTo(other: Int): Long = abs(this.toLong() - other.toLong())
+fun Int.splitInHalfOrNull(): Pair<Int, Int>? {
+    val str = this.toString()
+    if (str.length % 2 != 0) return null
+    val half = str.length / 2
+    return str.take(half).toInt() to str.substring(half).toInt()
+}
